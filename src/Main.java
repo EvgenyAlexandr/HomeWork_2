7f081ae602a0db881b58abc0310f05b3b6c896d1
EvgenyAlexand <bragin-e@mail.ru>
2025-07-20 09:49:39 +0700

    Д/З - Выполнение - Выполнена задача 2.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задача 1
+        System.out.println("Задача 1");
+        var clientOS = 1;       // 0 — iOS, 1 — Android
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
+
+
+        // Задача 2
+        System.out.println("Задача 2");
+        var clientDeviceYear = 2015;
+        if (clientOS == 0) {            // iOS
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
+
+
+        // Задача 3
+        System.out.println("Задача 3");
 
     }
 }
